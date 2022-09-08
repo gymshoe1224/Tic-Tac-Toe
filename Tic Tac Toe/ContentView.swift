bdbfe8c81399969b8ed91d34ddbb51505114d1b9
Chris Markiewicz <cmarkiewicz@bsd220.org>
2022-09-08 12:40:40 -0500

    Added tap gesture mechanic

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -15,15 +16,20 @@ struct ContentView: View {
                 .bold()
                 .padding()
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Color.blue
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                            .frame(width: 120, height: 120, alignment: .center)
+                        .cornerRadius(30)
+                        .onTapGesture {
+                            moves[index] = "X"
+                        }
+                    
+                }
             }
         }
         .preferredColorScheme(.dark)
